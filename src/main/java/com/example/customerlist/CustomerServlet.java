b84c061616a4d5b0cf7ca904e4497ee8b65a42c8
lehoang12691 <lehoang12691>
2024-07-29 08:27:20 +0700

    edit url

--- src/main/java/com/example/customerlist/CustomerServlet.java
+++ src/main/java/com/example/customerlist/CustomerServlet.java
@@ -13,11 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 public class CustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("Mai Văn Hoàn", "1983-08-20", "Hà Nội", "path/to/image1.jpg"));
-        customers.add(new Customer("Nguyễn Văn Nam", "1983-08-21", "Bắc Giang", "path/to/image2.jpg"));
-        customers.add(new Customer("Nguyễn Thái Hòa", "1983-08-22", "Nam Định", "path/to/image3.jpg"));
-        customers.add(new Customer("Trần Đăng Khoa", "1983-08-17", "Hà Tây", "path/to/image4.jpg"));
-        customers.add(new Customer("Nguyễn Đình Thi", "1983-08-19", "Hà Nội", "path/to/image5.jpg"));
+        customers.add(new Customer("Mai Văn Hoàn", "1983-08-20", "Hà Nội", "image1.jpg"));
+        customers.add(new Customer("Nguyễn Văn Nam", "1983-08-21", "Bắc Giang", "image2.jpg"));
+        customers.add(new Customer("Nguyễn Thái Hòa", "1983-08-22", "Nam Định", "image3.jpg"));
+        customers.add(new Customer("Trần Đăng Khoa", "1983-08-17", "Hà Tây", "image4.jpg"));
+        customers.add(new Customer("Nguyễn Đình Thi", "1983-08-19", "Hà Nội", "image5.jpg"));
 
         request.setAttribute("customers", customers);
         request.getRequestDispatcher("index.jsp").forward(request, response);
